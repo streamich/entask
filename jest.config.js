fa3c54997d703d6314467cd7689b3799915ea56d
streamich <streamich@gmail.com>
2018-12-08 15:14:17 +0100

    test: 💍 make Jest work

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,7 @@
 module.exports = {
   verbose: true,
   testURL: 'http://localhost/',
-  setupFiles: ['<rootDir>/src/__tests__/setup.js'],
+  setupFiles: ['<rootDir>/__tests__/setup.js'],
   moduleFileExtensions: ['js'],
   transformIgnorePatterns: [],
   testRegex: '.*/__tests__/.*\.(test|spec)\.js$',
